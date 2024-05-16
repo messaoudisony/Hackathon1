95c9fe85b9789affcabf00054e0ef00a55c195b1
Loic legoff <loic95280@gmail.com>
2024-05-16 16:05:08 +0200

    update rate

--- src/components/Cabane.jsx
+++ src/components/Cabane.jsx
@@ -17,34 +17,40 @@ const TreeHouses = () => {
   };
 
   return (
-    <div className="container">
-      {data.housesData.map((house, index) => (
-        <div key={house.id} className="house-card">
-          <img
-            src={house.image_path}
-            alt={house.image_name}
-            className="house-image"
-          />
-          <div className="house-content">
-            <div className="house-title">{house.image_name}</div>
-            <div className="house-address">{house.address}</div>
+    <>
+      <h1>LOGEMENTS SPACIAUX</h1>
+      <div className="container">
+        {data.housesData.map((house, index) => (
+          <div key={house.id} className="house-card">
+            <img
+              src={house.image_path}
+              alt={house.image_name}
+              className="house-image"
+            />
+            <div className="house-content">
+              <div className="house-title">{house.image_name}</div>
+              <div className="house-address">{house.address}</div>
 
-            <div className="house-description">
-              {isDescriptionReduced[index]
-                ? `${house.description.slice(0, 50)}...`
-                : house.description}
-              <button
-                className="buttonDisplay"
-                onClick={() => toggleDescription(index)}
-              >
-                {isDescriptionReduced[index] ? "...plus" : "...moins"}
-              </button>
+              <div className="house-description">
+                {isDescriptionReduced[index]
+                  ? `${house.description.slice(0, 50)}...`
+                  : house.description}
+                <button
+                  className="buttonDisplay"
+                  onClick={() => toggleDescription(index)}
+                >
+                  {isDescriptionReduced[index] ? "plus" : "moins"}
+                </button>
+              </div>
+            </div>
+            <div className="house-price">
+              <div>{house.price} €</div>
+              <div>⭐{house.rate}</div>
             </div>
           </div>
-          <div className="house-price">{house.price} €</div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
