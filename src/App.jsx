ddd41aa1ff24c33737545cd9fd1c9f6d6b818778
messaoudisony <messaoudi.sonia89@gmail.com>
2024-05-16 17:10:41 +0200

    joint components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Caroussel from "./componenets/caroussel/Caroussel";
-import NavbarToggle from "./componenets/navbar/NavbarToggle";
+
 
  function App() {
   return (
     <>
-      <NavbarToggle />
-      <Caroussel />
+      
     </>
   )
 }
