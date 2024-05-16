dd7641bbff78db14586c8b20dd066a97353c12f2
mohamaddayem <u@example.com>
2024-05-16 16:31:57 +0300

    routing

---
+++ src/component/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>here is the about us</h1>
+      <p>this is who we are</p>
+      
+      
+    </div>
+  );
+};
+
+export default AboutUs;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,78 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from "./component/Home";
+import Cabane from "./component/Cabane";
+import Hobbit from "./component/Hobbit";
+import AboutUs from "./component/AboutUs";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Hobbit">Hobbit</Link>
+          <Link to="/AboutUs">About Us</Link>
+        </nav>
+        <main>
+          <Home />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/Hobbit",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Cabane">Cabane</Link>
+          <Link to="/AboutUs">About Us</Link>
+        </nav>
+        <main>
+          <Hobbit />
+        </main>
+      </>
+    ),
+  },
+  {
+    path: "/Cabane",
+    element: (
+      <>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Hobbit">Hobbit</Link>
+          <Link to="/AboutUs">About Us</Link>
+        </nav>
+        <main>
+          <Cabane />
+        </main>
+      </>
+    ),
+  },
+{
+  path:"/AboutUs",
+  element:(
+    <>
+    <nav>
+    <Link to="/">Home</Link>
+    <Link to="/Hobbit">Hobbit</Link>
+    <Link to="/Cabane">Cabane</Link>
+    </nav>
+    <main>
+      <AboutUs/>
+    </main>
+  
+    
+    </>
+  )
+}
+]);
+
+// rendering
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
