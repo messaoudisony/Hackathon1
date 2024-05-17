202e4bef0013d971d126cd15a129f71b592856ae
Alp Hamurcu <alp_hamurcu@yahoo.com>
2024-05-17 09:08:23 +0200

    blabla

--- src/component/Hobbit.jsx
+++ src/component/Hobbit.jsx
@@ -1,8 +1,10 @@
 import HobbitHouses from "../components/Hobbit";
+import NavbarToggle from "./navbar/NavbarToggle";
 
 const Hobbit = () => {
   return (
     <>
+      <NavbarToggle />
       <HobbitHouses />
     </>
   );
