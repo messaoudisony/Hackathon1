ab5ab1e0dfbf240fbc815e784f087679322781bd
Loic legoff <loic95280@gmail.com>
2024-05-17 11:25:09 +0200

    add dev

--- src/pages/Cabane.jsx
+++ src/pages/Cabane.jsx
@@ -1,9 +1,9 @@
+import TreeHouses from "../components/Cabane";
 
 export default function Cabane() {
   return (
     <div>
-       <h1>here is the cabane</h1>
-      <p>welcome to the cabane</p>
+      <TreeHouses />
     </div>
-  )
+  );
 }
